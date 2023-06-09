df82fe0aeb7e680e9a60290a24f679b18ce6de9f
majdalkilany <majdkilany92@gmail.com>
2023-06-10 02:45:04 +0300

    Add the to do project strucure

---
+++ webpack.config.cjs
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve('./dist'),
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 8080,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
