5a92c15f59ffc8ae2f98ecc83868d4ca0628130f
majdalkilany <majdkilany92@gmail.com>
2023-06-23 16:13:00 +0300

    add tests for remove & make mock data and function e,plement dom in jest

--- __tests__/add&remove.test..js
+++ __tests__/add&remove.test..js
@@ -76,4 +76,31 @@ describe('deleteTask', () => {
     expect(updatedTaskArray).toHaveLength(1);
     expect(updatedTaskArray).not.toContain(arr[1]);
   });
+  test('Removing an item from DOM', () => {
+    document.body.innerHTML = `
+      <form class="form">
+        <input class="flex-cell form-input" id="input-description" placeholder="Add to your list" value="majd" required="">
+      </form>
+      <ul id="sub-container">
+        <li class="flex-row draggable" data-index="0" draggable="true">
+          <div class="flex-cell">
+            <span class="check-mark" id="check-mark">:heavy_check_mark:</span>
+            <button class="check-btn"></button>
+          </div>
+          <div class="flex-cell">study</div>
+          <textarea class="flex-cell  hidden">study</textarea>
+          <div class="flex-cell">
+            <div class="vertical-dots">
+              <div class="dot"></div>
+              <div class="dot"></div>
+              <div class="dot"></div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    `;
+    deleteTask2(1);
+    const myList = document.querySelector('#sub-container');
+    expect(myList.children).toHaveLength(0);
+  });
 });
